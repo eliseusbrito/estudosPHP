8766678279b873237d7c52ef4691526271c01065
Eliseu Brito <eliseu.brito@fiergs.org.br>
2023-03-28 10:59:21 -0300

    Adicionado argumentos na classe  migration create vehicles e rodado comando php artisan migrate para criar tabela no BD

--- crudPhp/laravel-crud/database/migrations/2023_03_28_134135_create_vehicles_table.php
+++ crudPhp/laravel-crud/database/migrations/2023_03_28_134135_create_vehicles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
+            $table->string("name", 50);
+            $table->integer("year");
+            $table->string("color", 30);
             $table->timestamps();
         });
     }
